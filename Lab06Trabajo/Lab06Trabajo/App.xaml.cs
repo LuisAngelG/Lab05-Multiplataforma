aaaa83ac7645feb5b04ac306d5865c295ca3091c
LuisAngelG <luis.gastelu@tecsup.edu.pe>
2023-04-29 20:21:36 -0500

    Agregar archivos de proyecto.

---
+++ Lab06Trabajo/Lab06Trabajo/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Lab06Trabajo
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new MainPage());
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
